f3cb00faab61e472ccb22b9001cf758e1dbc9325
mewturn <pathonice@hotmail.com>
2018-10-09 11:09:50 +0800

    Add exit to loop

--- movement.py
+++ movement.py
@@ -2,7 +2,7 @@ if __name__ == "__main__":
     x, y = 0, 0
     steps = 0
     while True:
-        dir = input('Your current position is %s, %s, where would you like to move to?' % (str(x), str(y)))
+        dir = input('Your current position is %s, %s, where would you like to move to? ' % (str(x), str(y)))
         
         directions = {  'north': (0, 1), 
                         'south' : (0, -1), 
@@ -15,8 +15,9 @@ if __name__ == "__main__":
             y += directions[dir][1]
             steps += 1
             
-        elif dir == "leave"
+        elif dir == "leave":
             print("Goodbye. You moved %s steps in total." % steps)
+            break
         
         else:
             print("You tried to move %s but that is not possible." % dir)
